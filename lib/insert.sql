42e9bbeb6d179c061513d12d72b00b6dcde18851
JuniperRhy <gwyniver.rhy@gmail.com>
2021-07-22 17:35:18 -0500

    commit tree 02ad702995a1b43702f80c49920396d9324c7e44

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr.Chocolate', 300, "F", "Brown", "Pretty chill", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 12, "F", "Brown", "Pretty chill", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 42, "M", "Polar", "Supes not chill", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 18, "F", "Sun", "Hardass", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 6, "M", "Brown", "Cute, but is angert", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 55, "M", "Green?", "Wait, is this one actually a bear...?", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 36, "M", "Black", "Meh", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 36, "M", "Black", "Heckin' killed Tim, WHAT DO YOU THINK?!", false);
